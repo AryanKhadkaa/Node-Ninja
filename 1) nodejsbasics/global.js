944d38c058527e026790f8856d5edeb9a4de4cbb
AryanKhakdaa <khadkaaryan809@gmail.com>
2025-01-24 04:03:48 +0545

    First two lessons of Node JS series

---
+++ 1) nodejsbasics/global.js
@@ -0,0 +1,10 @@
+// Global Object
+
+//console.log(global)
+
+// global.setTimeout(() => {
+//     console.log('in the timeout')
+// }, 3000);
+
+console.log(__dirname); //global
+console.log(__filename); //global  
---
+++ 1) nodejsbasics/people.js
@@ -0,0 +1,11 @@
+
+const people = ['aruyan', 'biraj','nisal'];
+const ages = [10,20,30];
+
+console.log(people);
+
+module.exports ={
+    People:people,
+    Ages: ages
+}
+ 
