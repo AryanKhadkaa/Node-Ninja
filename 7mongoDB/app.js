c75c5de4d0339af0a4dbc1a1d72fdd9bf7e55056
AryanKhakdaa <khadkaaryan809@gmail.com>
2025-01-29 00:00:01 +0545

    Complete lesson 7

--- 7mongoDB/app.js
+++ 7mongoDB/app.js
@@ -1,5 +1,6 @@
-// we will connect to mongoDB via mongoose and complete the blog section that we started in lesson 5 viewengines
 
+
+// we will connect to mongoDB via mongoose and complete the blog section that we started in lesson 5 viewengines
 
 
 const express = require('express')
@@ -9,7 +10,7 @@ const Blog = require('./models/blogs');
 const app = express();
 
 //connect to mongoDB
-const dbURI = "mongodb+srv://aryankhadka:heroaryankdka00@cluster0.ec782.mongodb.net/test";
+const dbURI = "mongodb+srv://aryankhadka:heroaryankdka00@cluster0.ec782.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(dbURI) // connection to the particular DB
 // this is actually asynchornous, and returns a promise
 .then((result)=> app.listen(3000) ) // we start listening for res only if DB connection is established
@@ -17,6 +18,8 @@ mongoose.connect(dbURI) // connection to the particular DB
 
  // now we need to create a schema and a model for our blogs in blog.js
 app.set('view engine', 'ejs');
+
+//routes
 
 app.get('/',(req,res)=>{
   const blogs = [
@@ -27,11 +30,25 @@ app.get('/',(req,res)=>{
   res.render('index',{title:'Home',blogs})
 })
 
+app.get('/blogs',(req,res)=>{
+  Blog.find().sort({createdAt: -1}) //filtering b created date and -1 indicated descending order
+  .then((result)=>{
+    console.log(result)
+    res.render('blogs',{title:'Blogs', blogs:result})
+
+  })
+  .catch((err)=>console.log(err))
+
+})
  // now that we have created a schema and model for our blog, we need to get and save data from and to DB
 
 
  // mongoose and mongo sandbox routes
 
+
+ //blog routes
+
+ // to send data to DB
   app.get('/createBlogs',(req,res)=>{
     console.log(req.url)
        const blog = new Blog({
@@ -39,12 +56,32 @@ app.get('/',(req,res)=>{
         snippet:'new blogs',
         body:'new blog',
        })
-       blog.save()
-       .then((result)=>{
+       blog.save()  // this is an  asynchrounous task, it takes some time and returns a promise
+       .then((result)=>{ // so we use a then method, which will fire a callback fucntion when this promise resolves (fulfills)
         console.log('yes')
         res.send(result)
        })
-       .catch((err)=>{
+       .catch((err)=>{ // catch method for errors
         console.log(err)
        })
+  })
+
+  // to retrieve data from DB
+
+  app.get('/createBlogs',(req,res)=>{
+    Blog.find() //gives aus all the docs in our collection i.e blogs and this is also asynchornous and gives us a promies
+    .then((result)=>{
+      res.send(result);
+    })
+    .catch((err)=>console.log(err))
+  })
+
+  // to retrieve a single data
+
+  app.get('/singleBlog',(req,res)=>{
+    Blog.findById("679911020a8cf5c3eb7f0a4f") //finding a particular data in collection by ID
+    .then((result)=>
+    res.send(result))
+    .catch((err)=>
+    console.log(err))
   })
